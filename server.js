fa8458c6b32ad4a38433c670e2e3dba09a99abb4
dhavaljpatel101 <dhavaljpatel@gmail.com>
2017-05-11 07:49:56 +0000

    Session3

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
 var app = express();
 app.use(bodyParser.json());
+app.use(express.static('public'));
 mongoose.connect('mongodb://localhost:27017/mittens');
+
+var Meow = mongoose.model('Meow', {
+ text: String
+  });
+
+app.get('/meows', function(req, res, next){
+  Meow.find({}, function(err, meows){
+    return res.json(meows);
+});
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
