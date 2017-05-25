deeb169ee4ca2408a43b1f3081a8e9eaaab1f6ed
Dhaval Patel <dhavaljpatel101@gmail.com>
2017-05-25 06:08:33 +0000

    CheckingPushFunctionality

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/meows', function(req, res, next){
 });
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(4000, function () {
+  console.log('Example app listening on port 4000!')
 });
 
